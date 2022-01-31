2eb711f990bcad3ecc1e77248c9051ad579fce4b
Adam Henriksson <adam.ingemar.henriksson@gmail.com>
2022-01-31 21:10:14 +0100

    Added unittests

--- fuv.py
+++ fuv.py
@@ -1,5 +1,38 @@
-from types import Parameters
+from typing import List
+import unittest
 
 
 def create_fuv(puv: List[bool], pum: List[List[bool]]):
-  return list(not puv[i] or all(pum[i]) for i in range(len(puv)))
+    return list(not puv[i] or all(pum[i]) for i in range(len(puv)))
+
+
+class FUVTest(unittest.TestCase):
+    def test_all_true(self):
+        puv = [True,
+               True,
+               True]
+        pum = [[True, True, True],
+               [True, True, True],
+               [True, True, True]]
+        fuv = create_fuv(puv, pum)
+        assert all(fuv)
+
+    def test_all_false(self):
+        puv = [False,
+               False,
+               False]
+        pum = [[False, False, False],
+               [False, False, False],
+               [False, False, False]]
+        fuv = create_fuv(puv, pum)
+        assert all(fuv)
+
+    def test_mixed(self):
+        puv = [False,
+               True,
+               True]
+        pum = [[False, False, True],
+               [True, True, True],
+               [True, False, True]]
+        fuv = create_fuv(puv, pum)
+        assert fuv == [True, True, False]
